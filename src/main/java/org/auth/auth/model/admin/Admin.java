00dee0ccffee332c170b48d2019b1cfb65123d19
matoussi roua <roua.mattoussi@ensi-uma.tn>
2025-02-09 23:46:59 +0100

    class admin and userentity added , configure application.yml

---
+++ src/main/java/org/auth/auth/model/admin/Admin.java
@@ -0,0 +1,22 @@
+package org.auth.auth.model.admin;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.auth.auth.model.user.UserEntity;
+
+@AllArgsConstructor
+@Table(name = "admin")
+@Entity
+@Getter
+@Setter
+@ToString
+@DiscriminatorValue("ADMIN")
+
+public class Admin extends UserEntity {
+
+}
---
+++ src/main/java/org/auth/auth/model/user/UserEntity.java
@@ -0,0 +1,84 @@
+package org.auth.auth.model.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.UUID;
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "user_entity")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "user_type")
+public abstract class UserEntity implements UserDetails {
+
+    @GeneratedValue
+    @Column(name = "id" , nullable = false , unique = true)
+    @Id
+    private UUID id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email" , nullable = false , unique = true)
+    private String email;
+
+    @Column(name = "password" , nullable = false )
+    private String password;
+
+    @Column(name = "is_enabled")
+    private boolean enabled;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_account_expired")
+    private boolean accountNonExpired;
+
+    @Column(name = "is_account_locked")
+    private boolean accountNonLocked;
+
+    @Column(name = "is credential_expired")
+    private boolean credentialsNonExpired;
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return null;
+    }
+
+
+}
