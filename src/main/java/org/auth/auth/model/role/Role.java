c324831ac79e275c738b796773d39f1920553e39
matoussi-roua <roua.mattoussi@ensi-uma.tn>
2025-02-10 10:17:45 +0100

    class role added

---
+++ src/main/java/org/auth/auth/model/role/Role.java
@@ -0,0 +1,37 @@
+package org.auth.auth.model.role;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+@ToString
+
+public class Role {
+
+    @SequenceGenerator(
+            name = "role_sequence",
+            sequenceName = "role_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "role_sequence")
+    @Column(name = "id_role")
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+
+    }
+}
