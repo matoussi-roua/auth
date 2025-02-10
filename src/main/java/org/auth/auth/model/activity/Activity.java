bf5e1ea5e5c18159a90532caa2672725a1087426
matoussi-roua <roua.mattoussi@ensi-uma.tn>
2025-02-10 13:59:21 +0100

    class activity added

---
+++ src/main/java/org/auth/auth/model/activity/Activity.java
@@ -0,0 +1,37 @@
+package org.auth.auth.model.activity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Table(name = "activities")
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_activity", nullable = false, updatable = false , unique = true)
+    private Long id;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "note")
+    private String note;
+
+    @Column(name = "documents")
+    private String documents;
+}
