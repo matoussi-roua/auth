058dfed3e55ca8fdd1dd2588fb9bdec591141954
matoussi-roua <roua.mattoussi@ensi-uma.tn>
2025-02-10 10:42:45 +0100

    class adress added

--- src/main/java/org/auth/auth/model/address/Adress.java
+++ src/main/java/org/auth/auth/model/address/Adress.java
@@ -1,4 +1,37 @@
 package org.auth.auth.model.address;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.print.attribute.standard.MediaSize;
+
+@Entity
+@Table(name = "adress")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Adress {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_adress")
+    private Long id;
+
+    @Column(name = "adress")
+    private String adress;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
 }
